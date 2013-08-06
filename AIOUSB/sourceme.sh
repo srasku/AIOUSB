151dbee3116dddca183d01640746074e667ef6be
Jimi Damon <jdamon@accesio.com>
2013-08-06 16:11:52 -0700

    BUG: more jenkins stuff

--- AIOUSB/sourceme.sh
+++ AIOUSB/sourceme.sh
@@ -2,6 +2,6 @@
 # Setups basic paths
 
 export AIOUSB_ROOT="$(pwd)"
-export AIO_LIB_DIR="AIOUSB_ROOT/lib"
+export AIO_LIB_DIR="${AIOUSB_ROOT}/lib"
 export AIO_CLASSLIB_DIR="$AIOUSB_ROOT/classlib"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${AIOUSB_ROOT}/lib:${AIOUSB_ROOT}/classlib"
