b2aaafc477c681185fffd4d4487e4fe45e9ae175
Jimi Damon <jdamon@accesio.com>
2013-08-06 15:59:57 -0700

    BUG: still racking head trying to fix jenkins

--- AIOUSB/sourceme.sh
+++ AIOUSB/sourceme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Setups basic paths
 
-export AIOUSB_ROOT=$(pwd)
-export AIO_LIB_DIR=$AIOUSB_ROOT/lib
-export AIO_CLASSLIB_DIR=$AIOUSB_ROOT/classlib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${AIOUSB_ROOT}/lib:${AIOUSB_ROOT}/classlib
+export AIOUSB_ROOT="$(pwd)"
+export AIO_LIB_DIR="AIOUSB_ROOT/lib"
+export AIO_CLASSLIB_DIR="$AIOUSB_ROOT/classlib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${AIOUSB_ROOT}/lib:${AIOUSB_ROOT}/classlib"
