85eae31fe48ff6af60c00bd1f98d397ae2bbf333
Jimi Damon <jdamon@accesio.com>
2013-08-07 13:22:50 -0700

    BUG: Fix to exit build early in case of failure

--- scripts/build_utilities.sh
+++ scripts/build_utilities.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+BUILD_LOG_TIME_FORMAT='+<%m/%d/%Y %H:%M:%S>'
 
-function build_cmd {
-    echo "# $(date '+<%m/%d/%Y %H:%M:%S>') $@" | tee -a ${BUILD_LOG};
-    eval $@ 2>&1 | tee -a ${BUILD_LOG};
+function LOG_TIME
+{
+    echo $(date "${BUILD_LOG_TIME_FORMAT}")
 }
 
+function build_cmd {
+    echo "# $(LOG_TIME) $@" | tee -a ${BUILD_LOG};
+    eval $@ 2>&1 | tee -a ${BUILD_LOG};
+    if [ $? != 0  ] ; then
+        echo "$(LOG_TIME) Error with '$@'"
+        exit 1
+    fi
+}
+
+
 function build_cd {
-    echo "# $(date '+<%m/%d/%Y %H:%M:%S>') cd $@" | tee -a ${BUILD_LOG};
+    echo "# $(LOG_TIME) cd $@" | tee -a ${BUILD_LOG};
     cd $@
 }
 
 
 function start_build {
-    echo "# $(date '+<%m/%d/%Y %H:%M:%S>')  Starting build" | tee -a ${BUILD_LOG}
+    echo "# $(LOG_TIME)  Starting build" | tee -a ${BUILD_LOG}
 }
