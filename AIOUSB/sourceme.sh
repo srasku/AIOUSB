e3556253e2560a36dc397b71c668ce8e8f45a6d9
Jimi Damon <jdamon@accesio.com>
2013-08-07 12:20:25 -0700

    ENH: Fix allows jenkins to run nightly builds

--- AIOUSB/sourceme.sh
+++ AIOUSB/sourceme.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Setups basic paths
 
-if [ -z $AIOUSB_ROOT ] ; then
-    export AIOUSB_ROOT="$(pwd)"
-fi
+export AIOUSB_ROOT="$(pwd)"
 export AIO_LIB_DIR="${AIOUSB_ROOT}/lib"
 export AIO_CLASSLIB_DIR="$AIOUSB_ROOT/classlib"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${AIOUSB_ROOT}/lib:${AIOUSB_ROOT}/classlib"
--- build.sh
+++ build.sh
@@ -9,5 +9,9 @@ export CPATH=/usr/include/libusb-1.0
 cd $AIO_LIB_DIR
 make
 cd $AIO_CLASSLIB_DIR
-make
+make 
 
+
+cd $AIO_LIB_DIR/wrappers
+
+prove tests/run_all_wrappers.t
