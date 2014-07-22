1fdc69317045a2e62260fa6b54d61501c35f7cea
Jimi Damon <jdamon@accesio.com>
2014-07-22 13:16:30 -0700

    REF: removed unnecessary dependency on htons

--- AIOUSB/lib/AIOUSB_DIO.c
+++ AIOUSB/lib/AIOUSB_DIO.c
@@ -8,11 +8,34 @@
 
 #include "AIOUSB_DIO.h"
 #include "AIOUSB_Core.h"
-#include <arpa/inet.h>
+
 
 #ifdef __cplusplus
 namespace AIOUSB {
 #endif
+
+
+                /* octaveOffset =  */
+
+/*----------------------------------------------------------------------------*/
+/**
+ * @desc Returns the number in Big-Endian format
+ */
+unsigned short aiousb_htons(unsigned short octaveOffset)
+{
+  short a;
+  char tmp1, tmp2;
+  a = 0xff;
+  tmp1 = *((char*)&a); 
+  if ( tmp1 == 0xff ) {	/* Little Endinan */
+    a = octaveOffset;
+    tmp1 = *((char*)&a); 
+    tmp2 = *(((char*)&a)+1);
+    *((char*)&a) = tmp2;
+    *(((char*)&a)+1) = tmp1;
+  }
+  return a;
+}         // oscillator wants the value in big-endian format
 
 /*----------------------------------------------------------------------------*/
 static unsigned short OctaveDacFromFreq(double *Hz) {
@@ -28,7 +51,7 @@ static unsigned short OctaveDacFromFreq(double *Hz) {
             } else {
                 offset = ( int )round(2048.0 - (ldexp(2078, 10 + octave) / *Hz));
                 octaveOffset = (( unsigned short )octave << 12) | (( unsigned short )offset << 2);
-                octaveOffset = htons(octaveOffset);         // oscillator wants the value in big-endian format
+                octaveOffset = aiousb_htons(octaveOffset); // oscillator wants the value in big-endian format
             }
           *Hz = (2078 << octave) / (2.0 - offset / 1024.0);
       }
