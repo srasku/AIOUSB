dc568a6428f9d14fbea1df5343afcbae2574ee16
Jimi Damon <jdamon@accesio.com>
2015-01-30 13:39:02 -0800

    DOC: doc of coutns and volts fifos

--- AIOUSB/lib/AIOFifo.c
+++ AIOUSB/lib/AIOFifo.c
@@ -217,7 +217,9 @@ AIORET_TYPE AIOFifoReadAllOrNone( AIOFifo *fifo, void *tobuf , unsigned maxsize 
     return actsize;
 }
 
+/* Counts Fifo definition */
 TEMPLATE_AIOFIFO_API( Counts, uint16_t );
+/* Volts Fifo definition */
 TEMPLATE_AIOFIFO_API( Volts, double );
 
 
