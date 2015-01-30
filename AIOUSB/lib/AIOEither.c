1b77f0b5a38b95abfefe81f161e5ef136e68b204
Jimi Damon <jdamon@accesio.com>
2015-01-30 12:01:24 -0800

    ENH: new internal error handling

---
+++ AIOUSB/lib/AIOEither.c
@@ -0,0 +1,235 @@
+/**
+ * @file   AIOEither.h
+ * @author $Format: %an <%ae>$
+ * @date   $Format: %ad$
+ * @version $Format: %t$
+ * @brief  General structure for AIOUSB Fifo
+ *
+ */
+
+#include "AIOEither.h"
+#include "AIOTypes.h"
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+namespace AIOUSB 
+{
+#endif
+
+#define LOOKUP(T) aioret_value_ ## T 
+
+void AIOEitherClear( AIOEither *retval )
+{
+    assert(retval);
+    switch(retval->type) { 
+    case aioeither_value_string:
+        free(retval->right.st);
+        break;
+    case aioeither_value_obj:
+        free(retval->right.v);
+        break;
+    default:
+        ;
+    }
+
+}
+    
+void AIOEitherSetRight(AIOEither *retval, AIO_EITHER_TYPE val , void *tmp, ... )
+{
+    assert(retval);
+    va_list ap;
+    switch(val) { 
+    case aioeither_value_int:
+        {
+            int t = *(int *)tmp;
+            retval->right.i = t;
+            retval->type = aioeither_value_int;
+        }
+        break;
+    case aioeither_value_unsigned:
+        {
+            unsigned t = *(unsigned*)tmp;
+            retval->right.u = t;
+            retval->type = aioeither_value_unsigned;
+        }
+        break;
+    case aioeither_value_double:
+        {
+            double t = *(double *)tmp;
+            retval->right.d = t;
+            retval->type = aioeither_value_double;
+        }
+        break;
+    case aioeither_value_string:
+        { 
+            char *t = *(char **)tmp;
+            retval->right.st = strdup(t);
+            retval->size     = strlen(t)+1;
+            retval->type     = aioeither_value_string;
+        }
+        break;
+    case aioeither_value_obj:
+        {    
+            va_start(ap, tmp);
+            int d = va_arg(ap, int);
+            va_end(ap);
+            retval->right.v = malloc(d);
+            retval->type = aioeither_value_obj;
+            retval->size = d;
+            memcpy(retval->right.v, tmp, d );
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void AIOEitherGetRight(AIOEither *retval, void *tmp, ... )
+{
+    assert(retval);
+    va_list ap;
+    switch(retval->type) { 
+    case aioeither_value_int:
+        {
+            int *t = (int *)tmp;
+            *t = retval->right.i;
+        }
+        break;
+    case aioeither_value_unsigned:
+        {
+            unsigned *t = (unsigned *)tmp;
+            *t = retval->right.u;
+        }
+        break;
+    case aioeither_value_double:
+        {
+            double *t = (double *)tmp;
+            *t = retval->right.d;
+        }
+        break;
+    case aioeither_value_string:
+        { 
+            memcpy(tmp, retval->right.st, strlen(retval->right.st)+1);
+        }
+        break;
+    case aioeither_value_obj:
+        {
+            va_start(ap, tmp);
+            int d = va_arg(ap, int);
+            va_end(ap);
+            memcpy(tmp, retval->right.v, d );
+        }
+        break;
+    default:
+        break;
+    }
+
+}
+
+void AIOEitherSetLeft(AIOEither *retval, int val)
+{
+    assert(retval);
+    retval->left = val;
+}
+
+int AIOEitherGetLeft(AIOEither *retval)
+{
+    return retval->left;
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef SELF_TEST
+
+#include "AIOUSBDevice.h"
+#include "AIOEither.h"
+#include "gtest/gtest.h"
+#include "tap.h"
+#include <iostream>
+using namespace AIOUSB;
+
+struct testobj {
+    int a;
+    int b;
+    int c;
+};
+
+TEST(AIOEither,BasicAssignments)
+{
+    AIOEither a = {0};
+    int tv_int = 22;
+
+    uint32_t tv_uint = 23;
+    double tv_double = 3.14159;
+    char *tv_str = (char *)"A String";
+    char readvals[100];
+    struct testobj tv_obj = {1,2,3};
+
+    AIOEitherSetRight( &a, aioeither_value_int32_t  , &tv_int );
+    AIOEitherGetRight( &a, readvals );
+    EXPECT_EQ( tv_int,  *(int*)&readvals[0] );
+    AIOEitherClear( &a );
+
+    AIOEitherSetRight( &a, aioeither_value_uint32_t , &tv_uint );
+    AIOEitherGetRight( &a, readvals );
+    EXPECT_EQ( tv_uint, *(uint32_t*)&readvals[0] );
+    AIOEitherClear( &a );
+
+    AIOEitherSetRight( &a, aioeither_value_double_t , &tv_double );
+    AIOEitherGetRight( &a, readvals );
+    EXPECT_EQ( tv_double, *(double*)&readvals[0] );
+    AIOEitherClear( &a );
+
+    AIOEitherSetRight( &a, aioeither_value_string, &tv_str );
+    AIOEitherGetRight( &a, readvals );
+    EXPECT_STREQ( tv_str, readvals );
+    AIOEitherClear( &a );
+
+    AIOEitherSetRight( &a, aioeither_value_string, &tv_str );
+    AIOEitherGetRight( &a, readvals );
+    EXPECT_STREQ( tv_str, readvals );
+    AIOEitherClear( &a );
+
+
+    AIOEitherSetRight( &a, aioeither_value_obj, &tv_obj , sizeof(struct testobj));
+    AIOEitherGetRight( &a, readvals, sizeof(struct testobj));
+    EXPECT_EQ( ((struct testobj*)&readvals)->a, tv_obj.a );
+    EXPECT_EQ( ((struct testobj*)&readvals)->b, tv_obj.b );
+    EXPECT_EQ( ((struct testobj*)&readvals)->c, tv_obj.c );
+    AIOEitherClear( &a );
+
+    AIOEitherSetRight( &a, aioeither_value_obj, &tv_obj , sizeof(struct testobj));
+    AIOEitherGetRight( &a, readvals, sizeof(struct testobj));
+    EXPECT_EQ( ((struct testobj*)&readvals)->a, tv_obj.a );
+    EXPECT_EQ( ((struct testobj*)&readvals)->b, tv_obj.b );
+    EXPECT_EQ( ((struct testobj*)&readvals)->c, tv_obj.c );
+    AIOEitherClear( &a );
+
+
+
+
+}
+
+int main(int argc, char *argv[] )
+{
+
+  AIORET_TYPE retval;
+
+  testing::InitGoogleTest(&argc, argv);
+  testing::TestEventListeners & listeners = testing::UnitTest::GetInstance()->listeners();
+#ifdef GTEST_TAP_PRINT_TO_STDOUT
+  delete listeners.Release(listeners.default_result_printer());
+#endif
+
+  listeners.Append( new tap::TapListener() );
+  return RUN_ALL_TESTS();  
+}
+
+#endif
+
+
---
+++ AIOUSB/lib/AIOEither.h
@@ -0,0 +1,68 @@
+/**
+ * @file   AIOEither.h
+ * @author $Format: %an <%ae>$
+ * @date   $Format: %ad$
+ * @version $Format: %t$
+ * @brief  General structure for returning results from routines
+ *
+ */
+#ifndef _AIOEITHER_H
+#define _AIOEITHER_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#ifdef __aiousb_cplusplus
+namespace AIOUSB {
+#endif
+
+
+typedef enum { 
+    aioeither_value_int = 1,
+    aioeither_value_int32_t = 1,
+    aioeither_value_uint32_t = 2,
+    aioeither_value_unsigned = 2,
+    aioeither_value_uint16_t = 3,
+    aioeither_vlaue_int16_t = 4,
+    aioeither_value_double_t = 5,
+    aioeither_value_double = 5,
+    aioeither_value_string = 6,
+    aioeither_value_obj,
+} AIO_EITHER_TYPE;
+
+typedef union { 
+    int i;
+    unsigned int u;
+    uint16_t us;
+    int16_t s;
+    double d; 
+    char *st;
+    void *v;
+} AIO_EITHER_VALUE_ITEM;
+
+typedef struct aio_ret_value  {
+    int left;
+    AIO_EITHER_VALUE_ITEM right;
+    AIO_EITHER_TYPE type;
+    int size;
+} AIOEither;
+
+
+void AIOEitherClear( AIOEither *retval );
+
+void AIOEitherSetRight(AIOEither *retval, AIO_EITHER_TYPE val , void *tmp, ... );
+void AIOEitherGetRight(AIOEither *retval, void *tmp, ... );
+
+void AIOEitherSetLeft(AIOEither *retval, int val );
+int  AIOEitherGetLeft(AIOEither *retval );
+
+
+
+#ifdef __aiousb_cplusplus
+}
+#endif
+
+
+
+#endif
